fc7ba6efa853121db56cae9fa6494f8ea682e864
kewagi <kewagi@icloud.com>
2018-09-26 14:49:38 +0200

    Added entrypoint

---
+++ bulwark-node/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/dash
+if ! grep rpcpassword /home/bulwark/.bulwark/bulwark.conf; then
+  RPCUSER="bulwarkrpc"
+  RPCPASSWORD=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  echo "rpcuser=${RPCUSER}" > /home/bulwark/.bulwark/bulwark.conf
+  echo "rpcpassword=${RPCPASSWORD}" >> /home/bulwark/.bulwark/bulwark.conf
+fi
+/bin/bulwarkd -datadir=/home/bulwark/data -conf=/home/bulwark/.bulwark/bulwark.conf
