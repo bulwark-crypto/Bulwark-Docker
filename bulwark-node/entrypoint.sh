a224f5575828558d059fa1e56700e981e56e1f5f
kewagi <kewagi@icloud.com>
2018-09-27 12:29:06 +0200

    Removed bulwark-node

--- bulwark-node/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/dash
-if ! grep rpcpassword /home/bulwark/.bulwark/bulwark.conf; then
-  RPCUSER="bulwarkrpc"
-  RPCPASSWORD=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-  echo "rpcuser=${RPCUSER}" > /home/bulwark/.bulwark/bulwark.conf
-  echo "rpcpassword=${RPCPASSWORD}" >> /home/bulwark/.bulwark/bulwark.conf
-fi
-/bin/bulwarkd
