b247d678aeaf08d3d2f58f9d4956533c506ef956
kewagi <kewagi@icloud.com>
2018-10-05 14:20:19 +0200

    Split bulwark into bulwark-build and bulwark-node

--- bulwark-build/entrypoint.sh
+++ bulwark-build/entrypoint.sh
@@ -4,10 +4,6 @@ if [ ! -f ~/.bulwark/bulwark.conf ]; then
   touch ~/.bulwark/bulwark.conf
 fi
 
-for ARG in "$@"; do
-  echo $ARG
-done
-
 if ! grep rpcpassword ~/.bulwark/bulwark.conf; then
   RPCUSER="bulwarkrpc"
   RPCPASSWORD=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
@@ -16,5 +12,9 @@ if ! grep rpcpassword ~/.bulwark/bulwark.conf; then
   echo "rpcpassword=${RPCPASSWORD}" >> ~/.bulwark/bulwark.conf
   echo "printtoconsole=1" >> ~/.bulwark/bulwark.conf
 fi
+
+for ARG in "$@"; do
+  echo "$ARG" >> ~/.bulwark/bulwark.conf
+done
 
 exec /bin/bulwarkd
