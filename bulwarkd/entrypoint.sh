80cd2a284e0ed7b0996aa295b61355df8b1e79b5
kewagi <kewagi@icloud.com>
2018-10-05 12:01:42 +0200

    Working node

--- bulwarkd/entrypoint.sh
+++ bulwarkd/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/dash
-if ! grep rpcpassword /home/bulwark/.bulwark/bulwark.conf; then
+echo "HELLO DEBUG"
+mkdir ~/.bulwark
+touch ~/.bulwark/bulwark.conf
+
+if ! grep rpcpassword ~/.bulwark/bulwark.conf; then
   RPCUSER="bulwarkrpc"
   RPCPASSWORD=$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-  echo "rpcuser=${RPCUSER}" >> /home/bulwark/.bulwark/bulwark.conf
-  echo "rpcpassword=${RPCPASSWORD}" >> /home/bulwark/.bulwark/bulwark.conf
+  touch ~/.bulwark/bulwark.conf
+  echo "rpcuser=${RPCUSER}" >> ~/.bulwark/bulwark.conf
+  echo "rpcpassword=${RPCPASSWORD}" >> ~/.bulwark/bulwark.conf
 fi
-/bin/bulwarkd
+/bin/bulwarkd -printtoconsole
