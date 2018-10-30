95d443478cb8cf7da4da9d737c8a7eb2d6f2e89f
kewagi <kewagi@bulwarkcrypto.com>
2018-10-30 11:01:23 +0100

    Updated organization name

--- bulwark-node/scripts/compose-gen.sh
+++ bulwark-node/scripts/compose-gen.sh
@@ -45,7 +45,7 @@ cat >> docker-compose.yml << EOL
       interval: 10m
       timeout: 30s
       retries: 3
-    image: kewagi/bulwark:latest
+    image: bulwarkcrypto/bulwark:latest
     networks:
       - ${NAMES[$i]}
     ports:
