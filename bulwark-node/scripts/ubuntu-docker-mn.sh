80ff8f3bc5756e0bebcb9ac93d5faecb5ef3f5a4
kewagi <kewagi@bulwarkcrypto.com>
2018-10-30 11:01:01 +0100

    Updated organization name

--- bulwark-node/scripts/ubuntu-docker-mn.sh
+++ bulwark-node/scripts/ubuntu-docker-mn.sh
@@ -94,7 +94,7 @@ cat >> docker-compose.yml << EOL
       interval: 10m
       timeout: 30s
       retries: 3
-    image: kewagi/bulwark:latest
+    image: bulwarkcrypto/bulwark:latest
     networks:
       - ${NAMES[$i]}
     ports:
