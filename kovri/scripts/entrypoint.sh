d7b27343347a4605190b1b526f004aeb12cf7dee
kewagi <kewagi@bulwarkcrypto.com>
2018-11-12 11:26:22 +0100

    Kovri now correctly copies keyfile

--- kovri/scripts/entrypoint.sh
+++ kovri/scripts/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/dash
 
 /usr/local/bin/kovri --host 0.0.0.0 --socksproxyaddress 0.0.0.0 -d &&
-mkdir /home/kovri/share &&
 sleep 3 &&
+mkdir -p /home/kovri/share &&
 cp /home/kovri/.kovri/client/keys/bulwarkd.dat.b32.txt /home/kovri/share/ &&
 tail -f /home/kovri/.kovri/logs/*.log
-fi
